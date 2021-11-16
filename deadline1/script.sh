50057bbd72e1ffd2586edb27f115f858f6f39f10
kit <kit@positive-sp-value.local>
2021-11-16 08:25:37 +0700

    Small fix

--- deadline1/script.sh
+++ deadline1/script.sh
@@ -2,6 +2,6 @@
 
 fastqc SRR15859207.fastq
 minimap2 -d ref.mmi ref.fna                       # indexing
-minimap2 -a ref.mmi reads.fastq > alignment.sam   # alignment
+minimap2 -a ref.mmi SRR15859207.fastq > SRR15859207.sam   # alignment
 samtools flagstat SRR15859207.sam > SRR15859207_flagstat.txt
 python main.py 
